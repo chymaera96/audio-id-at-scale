723bdcc17d09d12f40b0f0f3719253d4664d568e
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-08-06 19:54:07 +0100

    fixed: running tc_24

--- train.py
+++ train.py
@@ -86,11 +86,11 @@ class PLRectifiedFlow(pl.LightningModule):
     def configure_optimizers(self):
         return torch.optim.AdamW(self.parameters(), lr=self.config.lr)
 
-    # def on_train_epoch_end(self):
-    #     self.compute_metrics()
-
-    def validation_step(self, batch, batch_idx):
+    def on_train_epoch_end(self):
         self.compute_metrics()
+
+    # def validation_step(self, batch, batch_idx):
+    #     self.compute_metrics()
 
     def compute_metrics(self, num_samples=10000, num_steps=32):
         self.model.eval()
@@ -165,8 +165,8 @@ def train(config):
         precision=32,
     )
 
-    # trainer.fit(model, train_dataloaders=dataloader)
-    trainer.validate(model, dataloaders=dataloader)
+    trainer.fit(model, train_dataloaders=dataloader)
+    # trainer.validate(model, dataloaders=dataloader)
 
 
 if __name__ == "__main__":
