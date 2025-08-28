2fdd7d03ea8858fb1220b8a8e27b73230f9e8a69
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-08-28 13:33:03 +0100

    setup fixed

--- generate.sh
+++ generate.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 CHECKPOINT="audio-id-at-scale/tc28/checkpoints/epoch=99-step=361900.ckpt"
-OUT_PATH="/data/scratch/acw723/synth/GraFP/tc_29_epoch99_100K/dumm_db.mm"
+OUT_PATH="/data/scratch/acw723/synth/GraFP/tc_29_epoch99_100K/dummy_db.mm"
 
 for NUM_SAMPLES in "$@"; do
   KVAL=$(( NUM_SAMPLES / 1000 ))
   KSTR="${KVAL}K"
   OUT_CUR="${OUT_PATH//100K/${KSTR}}"
-#   mkdir -p "$(dirname "$OUT_CUR")"
+  mkdir -p "$(dirname "$OUT_CUR")"
 
   echo "Output path..."
   echo $OUT_CUR
 
 
-#   python generate.py --checkpoint="$CHECKPOINT" --num_samples="$NUM_SAMPLES" --out="$OUT_CUR"
+  python generate.py --checkpoint="$CHECKPOINT" --num_samples="$NUM_SAMPLES" --out="$OUT_CUR"
 done
