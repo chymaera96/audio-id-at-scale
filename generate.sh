30a00d56f7271d58c33475939ca43fed12123701
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-08-28 13:40:58 +0100

    logic check

--- generate.sh
+++ generate.sh
@@ -4,14 +4,20 @@ CHECKPOINT="audio-id-at-scale/tc28/checkpoints/epoch=99-step=361900.ckpt"
 OUT_PATH="/data/scratch/acw723/synth/GraFP/tc_29_epoch99_100K/dummy_db.mm"
 
 for NUM_SAMPLES in "$@"; do
-  KVAL=$(( NUM_SAMPLES / 1000 ))
-  KSTR="${KVAL}K"
-  OUT_CUR="${OUT_PATH//100K/${KSTR}}"
-  mkdir -p "$(dirname "$OUT_CUR")"
+  if [ "$NUM_SAMPLES" -lt 1000000 ]; then
+    VAL=$(( NUM_SAMPLES / 1000 ))
+    SUFFIX="K"
+  else
+    VAL=$(( NUM_SAMPLES / 1000000 ))
+    SUFFIX="M"
+  fi
+
+  STR="${VAL}${SUFFIX}"
+  OUT_CUR="${OUT_PATH//100K/${STR}}"
+#   mkdir -p "$(dirname "$OUT_CUR")"
 
   echo "Output path..."
-  echo $OUT_CUR
+  echo "$OUT_CUR"
 
-
-  python generate.py --checkpoint="$CHECKPOINT" --num_samples="$NUM_SAMPLES" --out="$OUT_CUR"
+#   python generate.py --checkpoint="$CHECKPOINT" --num_samples="$NUM_SAMPLES" --out="$OUT_CUR"
 done
