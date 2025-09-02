06191d3f9c7b3ad47575aa00e6b15ccae47bcab9
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-09-03 00:16:50 +0100

    generating bash code

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CHECKPOINT="audio-id-at-scale/tc28/checkpoints/epoch=99-step=361900.ckpt"
-OUT_PATH="/data/scratch/acw723/synth/GraFP/tc_29_epoch99_100K/dummy_db.mm"
+CHECKPOINT="/data/home/acw723/audio-id-at-scale/checkpoints/best/tc31-087.ckpt"
+OUT_PATH="/data/scratch/acw723/synth/GraFP/tc_27_epoch87_100K/dummy_db.mm"
 
 for NUM_SAMPLES in "$@"; do
   if [ "$NUM_SAMPLES" -lt 1000000 ]; then
