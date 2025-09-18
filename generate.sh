df1d7b287a1cabdd5e489ee79485563849eed950
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-09-18 11:58:45 +0100

    nmfp0 real scaling reprod

--- generate.sh
+++ generate.sh
@@ -21,9 +21,9 @@
 # python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_tc_27_370 --dummy_dir=/data/scratch/acw723/logs/emb/large/model_tc_27_370 --test_ids=500 --num_dummy=1000000 --iterations=20 --mean=-0.000263  --std=0.088348
 # python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_tc_27_370 --dummy_dir=/data/scratch/acw723/logs/emb/large/model_tc_27_370 --test_ids=500 --num_dummy=6000000 --iterations=20 --mean=-0.000263  --std=0.088348
 
-python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/synth/nmfp0_epoch97_10M  --test_ids=500 --num_dummy=10000 --iterations=20 --mean=-0.000410  --std=0.088388
-python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/synth/nmfp0_epoch97_10M  --test_ids=500 --num_dummy=50000 --iterations=20 --mean=-0.000410  --std=0.088388
-python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/synth/nmfp0_epoch97_10M  --test_ids=500 --num_dummy=100000 --iterations=20 --mean=-0.000410  --std=0.088388
-python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/synth/nmfp0_epoch97_10M  --test_ids=500 --num_dummy=500000 --iterations=20 --mean=-0.000410  --std=0.088388
-python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/synth/nmfp0_epoch97_10M  --test_ids=500 --num_dummy=1000000 --iterations=20 --mean=-0.000410  --std=0.088388
-python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/synth/nmfp0_epoch97_10M  --test_ids=500 --num_dummy=6000000 --iterations=20 --mean=-0.000410  --std=0.088388
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/logs/emb/large/model_nmfp  --test_ids=500 --num_dummy=10000 --iterations=20 --mean=-0.000410  --std=0.088388
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/logs/emb/large/model_nmfp  --test_ids=500 --num_dummy=50000 --iterations=20 --mean=-0.000410  --std=0.088388
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/logs/emb/large/model_nmfp  --test_ids=500 --num_dummy=100000 --iterations=20 --mean=-0.000410  --std=0.088388
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/logs/emb/large/model_nmfp  --test_ids=500 --num_dummy=500000 --iterations=20 --mean=-0.000410  --std=0.088388
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/logs/emb/large/model_nmfp  --test_ids=500 --num_dummy=1000000 --iterations=20 --mean=-0.000410  --std=0.088388
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_nmfp --dummy_dir=/data/scratch/acw723/logs/emb/large/model_nmfp  --test_ids=500 --num_dummy=6000000 --iterations=20 --mean=-0.000410  --std=0.088388
