f26ffe4679bf40b0ceb73d65c8c80a044c526f7e
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-09-18 02:09:43 +0100

    bash for benchmarking

--- generate.sh
+++ generate.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
 
-CHECKPOINT="/data/home/acw723/audio-id-at-scale/checkpoints/best/tc31-087.ckpt"
-OUT_PATH="/data/scratch/acw723/synth/GraFP/tc_27_epoch87_100K/dummy_db.mm"
-
-for NUM_SAMPLES in "$@"; do
-  if [ "$NUM_SAMPLES" -lt 1000000 ]; then
-    VAL=$(( NUM_SAMPLES / 1000 ))
-    SUFFIX="K"
-  else
-    VAL=$(( NUM_SAMPLES / 1000000 ))
-    SUFFIX="M"
-  fi
-
-  STR="${VAL}${SUFFIX}"
-  OUT_CUR="${OUT_PATH//100K/${STR}}"
-  mkdir -p "$(dirname "$OUT_CUR")"
-
-  echo "Output path..."
-  echo "$OUT_CUR"
-
-  python generate.py --checkpoint="$CHECKPOINT" --num_samples="$NUM_SAMPLES" --out="$OUT_CUR"
-done
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_peaknetfp --dummy_dir=/data/scratch/acw723/synth/pnfp0_epoch93_10M --test_ids=500 --num_dummy=10000 --iterations=20 --mean=0.033622 --std=0.081744 --tempo=auto
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_peaknetfp --dummy_dir=/data/scratch/acw723/synth/pnfp0_epoch93_10M --test_ids=500 --num_dummy=50000 --iterations=20 --mean=0.033622 --std=0.081744 --tempo=auto
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_peaknetfp --dummy_dir=/data/scratch/acw723/synth/pnfp0_epoch93_10M --test_ids=500 --num_dummy=100000 --iterations=20 --mean=0.033622 --std=0.081744 --tempo=auto
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_peaknetfp --dummy_dir=/data/scratch/acw723/synth/pnfp0_epoch93_10M --test_ids=500 --num_dummy=500000 --iterations=20 --mean=0.033622 --std=0.081744 --tempo=auto
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_peaknetfp --dummy_dir=/data/scratch/acw723/synth/pnfp0_epoch93_10M --test_ids=500 --num_dummy=1000000 --iterations=20 --mean=0.033622 --std=0.081744 --tempo=auto
+python benchmark.py --fp_dir=/data/scratch/acw723/logs/emb/large/model_peaknetfp --dummy_dir=/data/scratch/acw723/synth/pnfp0_epoch93_10M --test_ids=500 --num_dummy=6000000 --iterations=20 --mean=0.033622 --std=0.081744 --tempo=auto
